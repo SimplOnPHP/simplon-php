8cd967335a2c7984e925b1d7b86f6932bf0b0c59
Luca Lauretta <dubrox@gmail.com>
2011-06-05 18:35:09 -0500

    Trying to figure out how to commit this :s

--- sitio1/internal/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "includer.php";
-
-$class = $_REQUEST['class'] ?: 'Nota';
-
-$element = new $class();
-
-$element->PformElementGenerator('','save.php');
--- sitio1/internal/save.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "includer.php";
-
-
-$class = $_REQUEST['class'];
-
-$element = new $class();
-
-$element->fillFromRequest()->saveInDS();
