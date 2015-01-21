ce972dbe88885091a474349b1fbf97b8b5144955
dubrox <dubrox@gmail.com>
2015-01-20 23:01:46 -0600

    Just moved around

---
+++ Tutor/index.php
@@ -0,0 +1,38 @@
+<?php
+require '../SimplOn/Main.php';
+SimplOn\Main::run(array(
+
+	'DEFAULT_ELEMENT' => '\Web\Elements\Student',
+
+	'DEV_MODE' => true,
+
+	'DATA_STORAGE' => new SimplOn\DataStorages\MySql(
+		'localhost',
+		'tutorial',
+		'root',
+		'root'),
+
+	////////////////////////////////////////
+
+	'LOCAL_ROOT' => __DIR__,
+	'REMOTE_ROOT' => dirname($_SERVER['PHP_SELF']),
+	
+	'SimplOn_PATH' => realpath('../SimplOn'),
+	'GENERIC_TEMPLATES_PATH' => realpath('Templates'),
+	'MASTER_TEMPLATE' => realpath('../SimplOn').'/Renderers/Html5.html',
+
+	'DEFAULT_METHOD' => 'index',
+	'DEFAULT_RENDERER' => new SimplOn\Renderers\Html5(),
+
+	'CREATE_LAYOUT_TEMPLATES' => true,
+	'OVERWRITE_LAYOUT_TEMPLATES' => true,
+	'USE_LAYOUT_TEMPLATES' => true,
+	
+	'CREATE_FROM_TEMPLATES' => true,
+	'OVERWRITE_FROM_TEMPLATES' => true,
+	'USE_FROM_TEMPLATES' => true,
+	'JS_FLAVOUR' => 'jQuery',
+
+	'LIMIT_ELEMENTS' => '20',
+
+));
