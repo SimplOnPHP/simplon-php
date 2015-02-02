8c5fcfe6a47bb4896ccda418b49d31fb53620ebe
dubrox <dubrox@gmail.com>
2015-02-02 17:25:15 -0600

    Third step: added birth date and age fields.

--- Tutorial/Elements/Student.php
+++ Tutorial/Elements/Student.php
@@ -2,9 +2,11 @@
 namespace Tutorial\Elements;
 
 
+use SimplOn\Datas\Date;
 use SimplOn\Datas\ElementsContainer;
 use SimplOn\Datas\NumericId;
 use SimplOn\Datas\String;
+use SimplOn\Datas\TimeSince;
 use SimplOn\Elements\Element;
 
 class Student extends Element {
@@ -13,5 +15,8 @@ class Student extends Element {
         $this->first_name = new String('First name', 'S');
         $this->last_name = new String('Last name', 'S');
         $this->emails = new ElementsContainer(array(new EmailAddress()), 'Emails');
+
+        $this->birth_date = new Date('Birth date');
+        $this->age = new TimeSince('Age', $this->birth_date, '%y', 'L');
     }
 }
